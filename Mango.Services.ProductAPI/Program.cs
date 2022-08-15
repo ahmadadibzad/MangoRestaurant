166d8dff461b52f8bc9adc74d78dfd204bb3c5fc
ahmadadibzad <a.adibzad@gmail.com>
2022-08-15 15:06:11 +0430

    Implementaition of ProductRepository added.

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 IMapper mapper = MapppingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//
+// Dependency Injection
+//
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            var product = _mapper.Map<ProductDto, Product>(productDto);
+            
+            if (product.ProductId > 0)
+            {
+                _dbContext.Products.Update(product);
+            }
+            else
+            {
+                _dbContext.Products.Add(product);
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            try
+            {
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
+                if (product == null)
+                {
+                    return false;
+                }
+
+                _dbContext.Products.Remove(product);
+                await _dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<ProductDto> GetProductById(int productId)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            var mappedProducts = _mapper.Map<List<ProductDto>>(products);
+            return mappedProducts;
+        }
+    }
+}
