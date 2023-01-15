45e0627f33c44072b54888fb3983978678fe0a41
Ahmad Adibzad <102387865+ahmadadibzad@users.noreply.github.com>
2023-01-15 23:07:30 +0330

    Update Program.cs

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+// var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
